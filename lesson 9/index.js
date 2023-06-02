e6d429b9664ec08100f879975ba941746b74fada
temo1203 <temolortkipanidze2007@gmail.com>
2023-06-02 05:44:11 +0400

    good

--- lesson 9/index.js
+++ lesson 9/index.js
@@ -1,76 +1,47 @@
 $(function(){
-    var i = 1
     $('#first_click').click(function(){
+        $('*').removeClass('border')
+        $(this).addClass('border')
         $('.clicked_div').addClass('img1')
-        if($('#second_click').hasClass('border')){
-            $('#first_click').addClass('border')
-            $('#second_click').removeClass('border')
-        } else if($('#third_click').hasClass('border')){
-            $('#first_click').addClass('border')
-            $('#third_click').removeClass('border')
-        } else if($('#fourth_click').hasClass('border')){
-            $('#first_click').addClass('border')
-            $('#fourth_click').removeClass('border')
-        } else {
-            $('#second_click').addClass('border')
-            $('#first_click').removeClass('border')
-        }
+        $('.clicked_div').removeClass('img2 img3 img4')
     })
     $('#second_click').click(function(){
-        if($('#first_click').hasClass('border')){
-            $('#second_click').addClass('border')
-            $('#first_click').removeClass('border')
-        } else if($('#third_click').hasClass('border')){
-            $('#second_click').addClass('border')
-            $('#third_click').removeClass('border')
-        } else if($('#fourth_click').hasClass('border')){
-            $('#second_click').addClass('border')
-            $('#fourth_click').removeClass('border')
-        } else {
-            $('#second_click').removeClass('border')
-            $('#third_click').addClass('border')
-        }
+        $('.clicked_div').addClass('img2')
+        $('.clicked_div').removeClass('img1 img3 img4')
+        $('*').removeClass('border')
+        $(this).addClass('border')
+        
     })
     $('#third_click').click(function(){
-        if($('#first_click').hasClass('border')){
-            $('#third_click').addClass('border')
-            $('#first_click').removeClass('border')
-        } else if($('#second_click').hasClass('border')){
-            $('#third_click').addClass('border')
-            $('#second_click').removeClass('border')
-        } else if($('#fourth_click').hasClass('border')){
-            $('#third_click').addClass('border')
-            $('#fourth_click').removeClass('border')
-        } else {
-            $('#third_click').removeClass('border')
-            $('#fourth_click').addClass('border')
-        }
+        $('.clicked_div').addClass('img3')
+        $('.clicked_div').removeClass('img1 img2 img4')
+        $('*').removeClass('border')
+        $(this).addClass('border')
     })
     $('#fourth_click').click(function(){
-        if($('#first_click').hasClass('border')){
-            $('#fourth_click').addClass('border')
-            $('#first_click').removeClass('border')
-        } else if($('#second_click').hasClass('border')){
-            $('#fourth_click').addClass('border')
-            $('#second_click').removeClass('border')
-        } else if($('#third_click').hasClass('border')){
-            $('#fourth_click').addClass('border')
-            $('#third_class').removeClass('border')
-        } else {
-            $('#fourth_click').removeClass('border')
-            $('#third_click').addClass('border')
+        $('.clicked_div').addClass('img4')
+        $('.clicked_div').removeClass('img2 img3 img1')
+        $('*').removeClass('border')
+        $(this).addClass('border')
+    })
+    $('.search').change(function(){
+        var sr = $('.search')
+        var clicked = $('.clicked_div')
+        console.log(sr.val())
+        if(sr.val() == 'img1'){
+            $('.clicked_div').removeClass('img2 img3 img4')
+          clicked.addClass('img1')
+        }else if(sr.val() == 'img2'){
+            $('.clicked_div').removeClass('img1 img3 img4')
+            clicked.addClass('img2')
+        }else if(sr.val() == 'img3'){
+            $('.clicked_div').removeClass('img1 img2 img4')
+            clicked.addClass('img3')
+        }else if(sr.val() == 'img4'){
+            $('.clicked_div').removeClass('img1 img2 img3')
+            clicked.addClass('img4')
+        }else {
+            alert('there is no such a element')
         }
     })
-    // $('#second_click').click(function(){
-    //     $(this).next().addClass("border")
-    //     $(this).removeClass('border')
-    // })
-    // $('#third_click').click(function(){
-    //     $(this).next().addClass("border")
-    //     $(this).removeClass('border')
-    // })
-    // $('#fourth_click').click(function(){
-    //     $(this).prev().addClass("border")
-    //     $(this).removeClass('border')
-    // })
 })
